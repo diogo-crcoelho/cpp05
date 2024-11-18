c240f77528ba9eaec1fad45303e827dcf357f1c0
Diogo Carvalho Da Rocha Coelho <dcarvalh@c2r9s9.42lisboa.com>
2024-11-18 11:53:39 +0000

    make commit

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 14:53:39 by dcarvalh          #+#    #+#             */
+/*   Updated: 2024/11/18 11:51:35 by dcarvalh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat :: Bureaucrat(): name("default"), grade(1){}
+
+Bureaucrat :: Bureaucrat(std :: string name, int grade): name(name), grade(grade){
+    if (this->grade < 1)
+        throw Bureaucrat :: GradeTooHighException();
+    else if(this->grade > 150)
+        throw Bureaucrat :: GradeTooLowException();
+}
+
+Bureaucrat :: Bureaucrat(Bureaucrat const&copied){
+    *this = copied;
+}
+             
+Bureaucrat &Bureaucrat :: operator=(Bureaucrat const&copied){
+    Bureaucrat *copy = new Bureaucrat(copied.name, copied.grade);
+    return *copy;
+}
+
+Bureaucrat :: ~Bureaucrat(){}
+
+int Bureaucrat :: getGrade() {return this->grade;}
+std :: string Bureaucrat ::getName() {return this->name;}
+
+void Bureaucrat :: incGrade(){
+    if (this->grade > 1)
+        --(this->grade);
+    else
+        throw Bureaucrat :: GradeTooHighException();
+}
+
+void Bureaucrat :: decGrade(){
+    if (this->grade < 150)
+        ++(this->grade);
+    else
+        throw Bureaucrat :: GradeTooLowException();
+}
+
+const char* Bureaucrat :: GradeTooHighException :: what() const throw() {
+    return "Grade is too high";
+}
+
+const char* Bureaucrat :: GradeTooLowException :: what() const throw() {
+    return "Grade is too Low";
+}
+
+std :: ostream &operator<<(std :: ostream& os, Bureaucrat& b)
+{
+    os << b.getName() << ", bureaucrat grade " << b.getGrade();
+    return os;
+}
---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcarvalh <dcarvalh@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 14:53:34 by dcarvalh          #+#    #+#             */
+/*   Updated: 2024/11/18 11:51:56 by dcarvalh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+
+class Bureaucrat {
+    public:
+        Bureaucrat();
+        Bureaucrat(std :: string, int);
+        Bureaucrat(Bureaucrat const&);
+        Bureaucrat &operator=(Bureaucrat const&);
+        ~Bureaucrat();
+    
+        std :: string getName();
+        int getGrade();
+
+        void incGrade();
+        void decGrade();
+
+        class GradeTooHighException : public std :: exception {
+            public:
+                virtual const char* what() const throw();
+        };
+        class GradeTooLowException : public std :: exception {
+            public:
+                virtual const char* what() const throw();
+        };
+        
+    private:
+        const std :: string name;
+        int grade;
+};
+
+std :: ostream &operator<<(std :: ostream &, Bureaucrat &);
+#endif
